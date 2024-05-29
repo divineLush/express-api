69581202243dc39b25cc9fe42b0fa91629608f6d
divineLush <alvipath@yahoo.com>
2024-05-29 18:04:08 +0700

    base controller class

---
+++ src/common/base.controller.ts
@@ -0,0 +1,27 @@
+import { LoggerService } from "../logger/logger.service"
+import { Router, Response } from 'express'
+import { IControllerRoute } from "./route.interface"
+
+export abstract class BaseController {
+  private readonly router: Router
+
+  constructor (private logger: LoggerService) {
+    this.router = Router()
+  }
+
+  ok<T>(res: Response, message: T) {
+    return this.send<T>(res, 200, message)
+  }
+
+  protected bindRoutes(routes: IControllerRoute[]) {
+    routes.forEach(r => {
+      this.logger.log(`[${r.method}]`)
+      this.router[r.method](r.path, r.func.bind(this))
+    })
+  }
+
+  private send<T>(res: Response, code: number, message: T) {
+    res.type('application/json')
+    return res.status(code).json(message)
+  }
+}

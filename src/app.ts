44207178bdb32a5c259ae26df0908efccbfafc46
divineLush <alvipath@yahoo.com>
2024-05-29 17:45:14 +0700

    basic dependency injection

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,16 @@
 import express, { Express } from 'express'
 import { userRoutes } from './users/users'
 import { Server } from 'http'
+import { LoggerService } from './logger/logger.service'
 
 export class App {
   app: Express = express()
   server: Server
   port: number = 8000
 
-  constructor() {
+  constructor(logger: LoggerService) {
     this.server = this.app.listen(this.port, () => {
-      console.log(`running on http://localhost:${this.port}`)
+      logger.log(`running on http://localhost:${this.port}`)
     })
   }
 
---
+++ src/logger/logger.service.ts
@@ -0,0 +1,17 @@
+import { Logger, ILogObj } from 'tslog'
+
+export class LoggerService {
+  private readonly logger: Logger<ILogObj> = new Logger()
+
+  log(...args: unknown[]) {
+    this.logger.info(...args)
+  }
+
+  error(...args: unknown[]) {
+    this.logger.error(...args)
+  }
+
+  warn(...args: unknown[]) {
+    this.logger.warn(...args)
+  }
+}
